f5949cc3bf63d2b01b5af8b011d22634a04f6598
Fabián <fabianveal@gmail.com>
2023-09-26 22:41:28 -0300

    chore(flake): add postgres and auth-server

--- flake.nix
+++ flake.nix
@@ -9,21 +9,98 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+
       in {
-        packages.hello = pkgs.hello;
+        packages = {
+          default = pkgs.hello;
+          auth-server = pkgs.poetry2nix.mkPoetryApplication {
+            projectDir = ./site/server/auth;
+            overrides = [ pkgs.poetry2nix.defaultPoetryOverrides ];
+          };
+        };
 
         devShell = pkgs.mkShell {
-          buildInputs = let 
-            nodePackages = pkgs.nodePackages;
-            haskellShell = with pkgs.haskellPackages; [
+          buildInputs = let
+            nixPackages = with pkgs; [ nixfmt ];
+            haskellPackages = with pkgs.haskellPackages; [
               haskell-language-server
               ghcid
               cabal-install
             ];
-            pythonShell = with pkgs; [ poetry ];
-            tsShell = with pkgs; [ nodejs-16_x ];
-            # elmShell = with pkgs; [ nodePackages.elm nodePackages.elm-land ];
-          in builtins.concatLists [ haskellShell pythonShell tsShell ];
+            pythonPackages = with pkgs; [ poetry ];
+            tsPackages = with pkgs; [ nodejs-16_x ];
+            # elmPackages = with pkgs; [ nodePackages.elm nodePackages.elm-land ];
+          in builtins.concatLists [
+            nixPackages
+            # haskellPackages
+            pythonPackages
+            # tsPackages
+          ];
+        };
+
+        apps = {
+          auth-server = {
+            type = "app";
+            program = let
+              script = pkgs.writeShellScriptBin "auth-server" ''
+                cd site/server/auth
+                poetry run manage runserver --noreload
+              '';
+            in "${script}/bin/auth-server";
+          };
+
+          postgres = {
+            type = "app";
+            program = let
+              script = pkgs.writeShellApplication {
+                name = "pg_start";
+                runtimeInputs = [ pkgs.postgresql ];
+                text = ''
+                  # Initialize a database with data stored in current project dir
+                  [ ! -d "./data/db" ] && initdb --no-locale -D ./data/db
+
+                  postgres -D ./data/db -k "$PWD"/data
+                '';
+              };
+            in "${script}/bin/pg_start";
+          };
+
+          createdb = {
+            type = "app";
+            program = let
+              script = pkgs.writeShellApplication {
+                name = "createdb";
+                runtimeInputs = [ pkgs.postgresql ];
+                text = ''
+                  # Create a database of your current user
+                  if ! psql -h "$PWD"/data -lqt | cut -d \| -f 1 | grep -qw "$(whoami)"; then
+                    createdb -h "$PWD"/data "$(whoami)"
+                  fi
+
+                  # Load DB dump
+                  psql -h "$PWD"/data < db.sql
+
+                  # Create configuration file for postgrest
+                  echo "db-uri = \"postgres://authenticator:mysecretpassword@localhost:5432/$(whoami)\"
+                  db-schemas = \"api\"
+                  db-anon-role = \"qiqe_user\"" > data/db.conf
+                '';
+              };
+            in "${script}/bin/createdb";
+
+            postgrest = {
+              type = "app";
+              program = let
+                script = pkgs.writeShellApplication {
+                  name = "postgrest";
+                  runtimeInputs = [ pkgs.postgrest ];
+                  text = ''
+                    postgrest data/db.conf
+                  '';
+                };
+              in "${script}/bin/postgrest";
+            };
+          };
         };
       });
 }
