317f8c5da70d6a67be76efa0f933804405495e85
Fabián <fabianveal@gmail.com>
2023-10-10 19:31:01 -0300

    feat(site/client/auth): add login and signup ui

--- site/client/auth/src/pages/login.tsx
+++ site/client/auth/src/pages/login.tsx
@@ -1,7 +1,21 @@
 export default function Login() {
   return (
-    <div>
+    <div id="login" className="container">
       <h1>Login</h1>
+
+      <form action="">
+        <label htmlFor="">
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div className="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
--- site/client/auth/src/pages/signup.tsx
+++ site/client/auth/src/pages/signup.tsx
@@ -1,7 +1,25 @@
 export default function Signup() {
   return (
-    <div>
+    <div id="signup" className="container">
       <h1>Signup</h1>
+
+      <form action="">
+        <label htmlFor="">
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <label htmlFor="">
+          <p>Confirm Password</p>
+          <input type="password" />
+        </label>
+        <div className="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
