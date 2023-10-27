c4cb60c198416217a00fef8a7cb4ecbe7f6df917
Fabián <fabianveal@gmail.com>
2023-10-26 23:35:59 -0300

    feat(flake/nix): add dev app to run all

--- flake.nix
+++ flake.nix
@@ -29,13 +29,17 @@
           });
         });
 
-        devEnv = pkgs.mkShell { buildInputs = let nixPackages = with pkgs; [ nixfmt ]; in nixPackages; };
+        devEnv = pkgs.mkShell {
+          buildInputs = let nixPackages = with pkgs; [ nixfmt ]; in nixPackages;
+        };
         mergeEnvs = pkgs: envs:
           pkgs.mkShell (builtins.foldl' (a: v: {
             buildInputs = a.buildInputs ++ v.buildInputs;
             nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-            propagatedBuildInputs = a.propagatedBuildInputs ++ v.propagatedBuildInputs;
-            propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ v.propagatedNativeBuildInputs;
+            propagatedBuildInputs = a.propagatedBuildInputs
+              ++ v.propagatedBuildInputs;
+            propagatedNativeBuildInputs = a.propagatedNativeBuildInputs
+              ++ v.propagatedNativeBuildInputs;
             shellHook = a.shellHook + "\n" + v.shellHook;
           }) (devEnv) envs);
 
@@ -70,6 +74,53 @@
           postgres = pkgs.qiqe.postgres;
           createdb = pkgs.qiqe.createdb;
           postgrest = pkgs.qiqe.postgrest;
+
+          dev = {
+            type = "app";
+            program = let
+              script = pkgs.writeShellApplication {
+                name = "dev";
+                runtimeInputs = [ pkgs.concurrently ];
+                text = ''
+                  echo "Starting development environment"
+                  echo "Press Ctrl+C to stop"
+                  echo ""
+
+                  # Start postgres
+                  ${apps.postgres.program} &
+                  # Save postgres pid
+                  postgres_pid=$!
+
+                  # Wait for postgres to start
+                  sleep 2
+
+                  npx concurrently \
+                    --names "auth-server,auth-client,interpreter"              \
+                    --prefix-colors "bgMagenta.bold,bgBlue.bold,bgYellow.bold" \
+                    --kill-others                                              \
+                    --success first                                            \
+                    --prefix "[{name}]"                                        \
+                    --prefix-length 3                                          \
+                    --timestamp-format "HH:mm:ss"                              \
+                    --timestamp-prefix "[{time}]"                              \
+                    --command                                                  \
+                      "echo 'Interpreter'; ${apps.interpreter.program}"        \
+                      "echo 'Auth-Server'; ${apps.auth-server.program}"        \
+                      "echo 'Auth-Client'; ${apps.auth-client.program}"
+
+                  echo "Stopping development environment"
+                  kill $postgres_pid
+                  result=$?
+                  if [ $result -eq 0 ]; then
+                    echo "Postgres stopped"
+                  else
+                    echo "Postgres failed to stop"
+                  fi
+                  exit $result
+                '';
+              };
+            in "${script}/bin/dev";
+          };
         };
       });
 }
