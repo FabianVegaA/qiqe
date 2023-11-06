d981ca69ab24550d43badb8698c82a6c3e6bc3b8
Fabián <fabianveal@gmail.com>
2023-11-05 22:57:43 -0300

    feat(site/client/auth): add home content

--- site/client/auth/src/pages/home.tsx
+++ site/client/auth/src/pages/home.tsx
@@ -1,7 +1,14 @@
+import MarkdownPage from "../components/markdown";
+
 export default function Home() {
+  const source =
+    "https://raw.githubusercontent.com/FabianVegaA/qiqe/main/README.md";
   return (
-    <div className="docs">
-      <h1>Welcome to Qiqe!</h1>
-    </div>
+    <>
+      <div className="docs">
+        <h1>Welcome to Qiqe!</h1>
+      </div>
+      <MarkdownPage source={source} className="docs" />;
+    </>
   );
 }
